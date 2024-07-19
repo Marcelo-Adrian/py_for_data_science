3d0771db0a7ce5db76315bbe3c5242c14ca15c6b
Marcelo-Adrian <marcelo.10vip@gmail.com>
2024-07-19 11:22:35 -0300

    Exp1

---
+++ Chapter_1/Exp1.py
@@ -0,0 +1,23 @@
+#1. Create a name and age variables, put your info in it.
+#2. Print "s, "x" ,\\"
+#3. Create a variable Pi and start it with the value 3.1415926 and print just two digits after the decimal point.
+#4. Print the value of (5*7)-((6*4)+(20/2))
+
+def main():
+    #1.
+    name ="Marcelo Adrian"
+    age = 22
+    print(name,age)
+
+    #2.
+    print("s, \"x\" \\\\")
+
+    3.
+    pi = 3.1415926
+    print(f"{pi:.2f}")
+
+    4.
+    print(f"(5*7)-((6*4)+(20/2)) = {(5*7)-((6*4)+(20/2))}")
+
+if __name__ == "__main__":
+    main()
